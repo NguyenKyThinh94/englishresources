636a0c739db1d5161184a9c670c9a75c5fa8a9bf
thinh.nguyen <kythinh94@gmail.com>
2021-01-14 08:40:03 +0700

    embed

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
 
 const Player = ({ url }) => {
-  return <ReactPlayer
-  className='react-player'
-  url={url}
-  controls={true}
-  width='100%'
-  height='100%'
-  />
+  return <iframe src={url} title='dddd' frameborder="0" scrolling="no" width="100%" height="100%" allowfullscreen control></iframe>
 }
 
 export default Player
